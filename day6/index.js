7a6d87feb4b61b65adf69fd38e0ed6be97c9448e
Eric Birdsall <eric@qualia.com>
2022-12-05 23:14:52 -0600

    add day 6

---
+++ day6/index.js
@@ -0,0 +1,16 @@
+const _ = require('underscore');
+const fs = require('fs');
+
+
+let text = fs.readFileSync('input.txt', {encoding:'utf8', flag:'r'});
+
+let markerSize = 4;
+
+for (let i = 0; i < text.length - markerSize; i++) {
+  let set = new Set(_.range(markerSize).map(j => text[i + j]));
+  if (set.size === markerSize) {
+
+    console.log(i + markerSize);
+    break;
+  }
+}
