2268aeee771e79b1665380c022740cbd7efb8a19
Eric Birdsall <eric@qualia.com>
2022-12-07 22:59:25 -0600

    add day 7

--- day7/index.js
+++ day7/index.js
@@ -1,12 +1,12 @@
 const _ = require('underscore');
 const fs = require('fs');
+const path = require('path');
 
-
-let text = fs.readFileSync('sample.txt', {encoding:'utf8', flag:'r'});
+let text = fs.readFileSync('input.txt', {encoding:'utf8', flag:'r'});
 let total = 0;
 
 
-currPath = ''
+let currPath = '/';
 
 let santaFS = {};
 
@@ -15,16 +15,9 @@ text.split('\n').forEach(line => {
   if (cmds[0] == '$') {
     if (cmds[1] == 'cd') {
       if (cmds[2] == '..') {
-        let newPath = currPath.split('/');
-        newPath.pop();
-        currPath = newPath.join('/');
+        currPath = path.dirname(currPath)
       } else {
-        if (currPath == '' || cmds[2] == '/') {
-          currPath = '/';
-        } else {
-          currPath = currPath + '/' + cmds[2];
-        }
-
+        currPath = path.join(currPath, cmds[2]);
       }
     } else if (cmds[1] == 'ls') {
       // do nothing
@@ -32,42 +25,31 @@ text.split('\n').forEach(line => {
   } else {
     if (cmds[0] == 'dir') {
       let folderName = cmds[1];
-      santaFS[currPath + '/' + folderName] = 'dir';
+      santaFS[path.join(currPath, folderName)] = 'dir';
     } else {
-      let fileSize = cmds[0];
-      let fileName = cmds[1];
-      santaFS[currPath + '/' + fileName] = fileSize;
+      let [fileSize, fileName] = cmds;
+      santaFS[path.join(currPath, fileName)] = Number(fileSize);
     }
   };
 });
 
 
 
-let newSantaFS = santaFS;
-// Object.keys(santaFS).forEach(key => {
-//   newSantaFS[key.substr(2, Infinity)] = santaFS[key];
-// });
-
-// console.log(JSON.stringify(newSantaFS, null, 2));
-
-let sum = {};
-let sum2 = 0;
-Object.keys(newSantaFS).forEach(key => {
-  console.log(key)
-  if (newSantaFS[key] == 'dir') {
-    let sizes = Object.keys(newSantaFS).map(smallkey => {
-      if (smallkey.startsWith(key) && !_.isNaN(+newSantaFS[smallkey])) return +newSantaFS[smallkey];
+let sum = 0;
+Object.keys(santaFS).forEach(key => {
+  if (santaFS[key] == 'dir') {
+    let sizes = Object.keys(santaFS).map(smallkey => {
+      if (smallkey.startsWith(key + '/') && santaFS[smallkey] !== 'dir') {
+        return santaFS[smallkey];
+      }
       return 0;
     });
 
     let size = sizes.reduce((a, b) => a + b, 0);
     if (size <= 100000) {
-      sum[key] = size;
-      sum2 += size;
+      sum += size;
     }
-    // console.log(`size of ${key}: ${size}`)
   }
 });
 
 console.log(sum);
-console.log(sum2)
---
+++ day7/index2.js
@@ -0,0 +1,63 @@
+const _ = require('underscore');
+const fs = require('fs');
+const path = require('path');
+
+let text = fs.readFileSync('input.txt', {encoding:'utf8', flag:'r'});
+let total = 0;
+
+
+let currPath = '/';
+
+let santaFS = {};
+
+text.split('\n').forEach(line => {
+  let cmds = line.split(' ');
+  if (cmds[0] == '$') {
+    if (cmds[1] == 'cd') {
+      if (cmds[2] == '..') {
+        currPath = path.dirname(currPath)
+      } else {
+        currPath = path.join(currPath, cmds[2]);
+      }
+    } else if (cmds[1] == 'ls') {
+      // do nothing
+    }
+  } else {
+    if (cmds[0] == 'dir') {
+      let folderName = cmds[1];
+      santaFS[path.join(currPath, folderName)] = 'dir';
+    } else {
+      let [fileSize, fileName] = cmds;
+      santaFS[path.join(currPath, fileName)] = Number(fileSize);
+    }
+  };
+});
+
+
+let sum = {};
+let sum2 = 0;
+
+let dirs = [];
+santaFS['/'] = 'dir'
+Object.keys(santaFS).forEach(key => {
+  if (santaFS[key] == 'dir') {
+    let sizes = Object.keys(santaFS).map(smallkey => {
+      if (smallkey.startsWith(key + '/') && santaFS[smallkey] !== 'dir') {
+        return santaFS[smallkey];
+      }
+      return 0;
+    });
+
+    let size = sizes.reduce((a, b) => a + b, 0);
+
+
+
+    if ((42586708 - size) < 40000000) {
+      dirs.push(size);
+
+    }
+
+  }
+});
+
+console.log(_.min(dirs));
